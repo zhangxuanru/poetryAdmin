726f41d805569bd4b4af5cccf4cde35fab3486a6
zhangxuanru <1107099011@qq.com>
2019-10-08 14:25:54 +0800

    时间搓

--- worker/app/tools/date.go
+++ worker/app/tools/date.go
@@ -14,7 +14,7 @@ import (
 func GetCurrentUnix() int64 {
 	var cstSh, _ = time.LoadLocation("Asia/Shanghai") //上海
 	now := time.Now().In(cstSh)
-	formStr := fmt.Sprintf("%d-%02d-%d %02d:%02d:%02d", now.Year(), now.Month(), now.Day(), 0, 0, 0)
+	formStr := fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d", now.Year(), now.Month(), now.Day(), 0, 0, 0)
 	parse, _ := time.ParseInLocation("2006-01-02 15:04:05", formStr, cstSh)
 	return parse.Unix()
 }
